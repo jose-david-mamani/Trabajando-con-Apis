ee752664b769abc2c7deee485630f5efd988fb0c
unknown <josemamanila51@gmail.com>
2026-06-10 14:40:05 -0300

    mi primer commit

---
+++ PrimerosPasos/src/PrimeraClase.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class PrimeraClase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("hola mundo");
+
+	}
+
+}
